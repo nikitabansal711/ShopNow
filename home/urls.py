f5317068733baf3f1c7650aa86c05012d376b88e
Nikita <nikita.bansal@mountblue.tech>
2020-09-25 17:45:03 +0530

    deploying on heroku

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.views.generic import TemplateView
-from django.contrib.auth import views as auth_views
+
 from social_login.views import GoogleLogin, GithubLogin
 
 urlpatterns = [
